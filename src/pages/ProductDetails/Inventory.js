8b9059979a90293bc97255417d1719dc668ee3f9
murshadahammed <ahammed.pb@experionglobal.com>
2022-12-27 11:50:05 +0530

    [REFACTOR] Re-orginzed Profile files

--- src/pages/ProductDetails/Inventory.js
+++ src/pages/ProductDetails/Inventory.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 import { useEffect, useRef, useState } from "react"
-import classes from './UserProfile.module.css';
+import classes from '../UserProfile/UserProfile.module.css';
 
 
 
